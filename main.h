f2f877c3432ed99177a2d752167f7aa13b15d74a
DevOse <abuluoisereme@gmail.com>
2023-01-24 19:02:03 -0800

    update

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -14,7 +15,6 @@
 #define F_HASH 8
 #define F_SPACE 16
 
-
 /* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
@@ -22,8 +22,8 @@
 /**
  * struct fmt - Struct op
  *
- * @fmt: The format
- * @fn: Funtion associated
+ * @fmt: The format.
+ * @fn: The function associated.
  */
 
 struct fmt
@@ -47,10 +47,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
-
-
 /* Funtions to print chars and strings */
-
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -59,7 +56,6 @@ int print_percent(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
-
 int print_int(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
@@ -80,29 +76,24 @@ int print_non_printable(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* Funcion to print memory address */
-
 int print_pointer(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
-
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
 /*Function to print string in reverse*/
-
 int print_reverse(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /*Function to print a string in rot 13*/
-
 int print_rot13string(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* width handler */
-
 int handle_write_char(char c, char buffer[],
 		int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
@@ -111,15 +102,16 @@ int write_num(int ind, char bff[], int flags, int width, int precision,
 		int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 		int width, int flags, char padd, char extra_c, int padd_start);
+
 int write_unsgnd(int is_negative, int ind,
 		char buffer[],
 		int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
-
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
+
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
